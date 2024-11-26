7901b8bca014a0b251ac639a1d032176b2d35a72
Grace MUGWANEZA <gracemugwaneza018@gmail.com>
2024-11-26 13:48:17 +0200

    feedback applied

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { OrderDto } from '../auth/dto';
 
